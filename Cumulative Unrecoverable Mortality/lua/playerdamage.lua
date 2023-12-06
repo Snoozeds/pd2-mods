97e097a99a24ef5ef9ec46956c5b59e8c72dfc1e
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2023-12-06 02:06:06 +0000

    Unnecessary returns

--- Cumulative Unrecoverable Mortality/lua/playerdamage.lua
+++ Cumulative Unrecoverable Mortality/lua/playerdamage.lua
@@ -1,12 +1,9 @@
 -- Block PlayerDamage healing
 function PlayerDamage:band_aid_health()
-    return
 end
 
 function PlayerDamage:recover_health()
-    return
 end
 
 function PlayerDamage:restore_health(...)
-    return
 end
